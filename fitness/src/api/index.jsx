e7e048b774fb728cfa2b261d9deb47442eb623ef
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-07 13:00:01 +0530

    commit

--- fitness/src/api/index.jsx
+++ fitness/src/api/index.jsx
@@ -1,24 +1,24 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://fitness-tracker-backend-fpdk.onrender.com/api/",
   });
   
 
-export const UserSignUp = async (data) => API.post("/user/signup", data);
-export const UserSignIn = async (data) => API.post("/user/signin", data);
+export const UserSignUp = async (data) => API.post("https://fitness-tracker-backend-fpdk.onrender.com/user/signup", data);
+export const UserSignIn = async (data) => API.post("https://fitness-tracker-backend-fpdk.onrender.com/user/signin", data);
 
 export const getDashboardDetails = async (token) =>
-  API.get("/user/dashboard", {
+  API.get("https://fitness-tracker-backend-fpdk.onrender.com/user/dashboard", {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const getWorkouts = async (token, date) =>
-  await API.get(`/user/workout${date}`, {
+  await API.get(`https://fitness-tracker-backend-fpdk.onrender.com/user/workout${date}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const addWorkout = async (token, data) =>
-  await API.post(`/user/workout`, data, {
+  await API.post(`https://fitness-tracker-backend-fpdk.onrender.com/user/workout`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
